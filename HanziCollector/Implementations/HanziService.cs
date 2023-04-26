30cf70237bec63697947fc61636c08fe9c2deb40
Hiep Tran <hiep.tran@zoi.de>
2023-04-26 15:27:29 +0700

    refactored logic

--- HanziCollector/Implementations/HanziService.cs
+++ HanziCollector/Implementations/HanziService.cs
@@ -9,116 +9,130 @@ namespace HanziCollector.Implementations;
 
 internal class HanziService : IHanziService
 {
-    private readonly ITextDocumentReader _textDocumentReader;
-    private readonly ICrawlerService _crawler;
-    private readonly IUnitOfWork _unitOfWork;
-    private readonly IHanziDbService _hanziDbService;
-    private readonly IMapper _mapper;
+  private readonly ITextDocumentReader _textDocumentReader;
+  private readonly ICrawlerService _crawler;
+  private readonly IUnitOfWork _unitOfWork;
+  private readonly IHanziDbService _hanziDbService;
+  private readonly IMapper _mapper;
 
-    public HanziService(ITextDocumentReader textDocumentReader, ICrawlerService crawlerService, IUnitOfWork unitOfWork, IHanziDbService hanziDbService, IMapper mapper)
+  public HanziService(ITextDocumentReader textDocumentReader, ICrawlerService crawlerService, IUnitOfWork unitOfWork,
+    IHanziDbService hanziDbService, IMapper mapper)
+  {
+    _textDocumentReader = textDocumentReader;
+    _crawler = crawlerService;
+    _unitOfWork = unitOfWork;
+    _hanziDbService = hanziDbService;
+    _mapper = mapper;
+  }
+
+  public async Task<IEnumerable<HanziFromHvDic>> ImportFromTextDocumentFile(string filePath, int takeFrom = 0,
+    int takeTo = 100)
+  {
+    List<HanziFromHvDic> result;
+    try
     {
-        _textDocumentReader = textDocumentReader;
-        _crawler = crawlerService;
-        _unitOfWork = unitOfWork;
-        _hanziDbService = hanziDbService;
-        _mapper = mapper;
-    }
+      var characters = _textDocumentReader.ReadToCharArray(filePath);
+      var chunk = characters
+        .Skip(takeFrom)
+        .Take(takeTo)
+        .ToList();
 
-    public async Task<IEnumerable<HanziFromHvDic>> ImportFromTextDocumentFile(string filePath, int takeFrom = 0, int takeTo = 100)
-    {
-        List<HanziFromHvDic> result;
-        try
-        {
-            var characters = _textDocumentReader.ReadToCharArray(filePath);
-            var chunk = characters
-                .Skip(takeFrom)
-                .Take(takeTo)
-                .ToList();
-            
-            var cleanedChunk = await RemoveDuplicatedHanzis(chunk);
-            
-            result = cleanedChunk.Select(async c => await _crawler.CrawlSingle(c))
-                .Select(t => t.Result)
-                .ToList();
-            
-            if (!result.Any())
-            {
-                return result;
-            }
+      var cleanedChunk = await RemoveDuplicatedHanzis(chunk);
 
-            foreach (var fromHvDic in result)
-            {
-                if (string.IsNullOrEmpty(fromHvDic.Pinyin) || string.IsNullOrEmpty(fromHvDic.HanViet))
-                {
-                    fromHvDic.Processed = false; 
-                    continue;
-                }
-                
-                var hanzi = _mapper.Map<Hanzi>(fromHvDic);
-                await _unitOfWork.Hanzis.Add(hanzi);
-                fromHvDic.Processed = true; 
-            }
+      result = cleanedChunk.Select(async c => await _crawler.CrawlSingle(c))
+        .Select(t => t.Result)
+        .ToList();
 
-            await _unitOfWork.CompleteAsync();
-        }
-        catch (IOException e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Unknown Exception: {e.Message}");
-            throw;
-        }
-
+      if (!result.Any())
+      {
         return result;
-    }
+      }
 
-    public async Task<HanziFromHvDic> GetHanziInformationFromHvDic(string hanzi)
-    {
-        var result = await _crawler.CrawlSingle(hanzi);
-        return result;
-    }
-
-    public async Task<Hanzi?> GetSingle(string id)
-    {
-        var result = await _unitOfWork.Hanzis.GetById(id);
-        return result;
-    }
-
-    public async Task<IEnumerable<Hanzi>> GetAllInDb()
-    {
-        return await _hanziDbService.ReadAll();
-    }
-
-    public async Task<IEnumerable<Hanzi>> GetAllInDb(int skip, int take)
-    {
-        return await _hanziDbService.ReadRange(skip, take);
-    }
-
-    public async Task<bool> Delete(string id)
-    {
-        var deleted = await _unitOfWork.Hanzis.Delete(id);
-        await _unitOfWork.CompleteAsync();
-        return deleted;
-    }
-
-    #region Private Methods
-
-    private async Task<IEnumerable<string>> RemoveDuplicatedHanzis(IEnumerable<string> ids)
-    {
-        var existingHanzis = await _hanziDbService.ReadAll();
-        var duplicatedOnes = existingHanzis
-            .Where(x => ids.Contains(x.Id)).ToList();
-        foreach (var duplicatedOne in duplicatedOnes)
+      foreach (var fromHvDic in result)
+      {
+        if (string.IsNullOrEmpty(fromHvDic.Pinyin) || string.IsNullOrEmpty(fromHvDic.HanViet))
         {
-            await _unitOfWork.Hanzis.Delete(duplicatedOne.Id);
+          fromHvDic.Processed = false;
+          continue;
         }
 
-        await _unitOfWork.CompleteAsync();
-        return ids;
+        var hanzi = _mapper.Map<Hanzi>(fromHvDic);
+        await _unitOfWork.Hanzis.Add(hanzi);
+        fromHvDic.Processed = true;
+      }
+
+      await _unitOfWork.CompleteAsync();
+    }
+    catch (IOException e)
+    {
+      Console.WriteLine(e);
+      throw;
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine($"Unknown Exception: {e.Message}");
+      throw;
     }
 
-    #endregion
+    return result;
+  }
+
+  public async Task<HanziFromHvDic> GetHanziInformationFromHvDic(string hanzi)
+  {
+    var result = await _crawler.CrawlSingle(hanzi);
+    return result;
+  }
+
+  public async Task<Hanzi?> GetSingle(string id)
+  {
+    var result = await _unitOfWork.Hanzis.GetById(id);
+    return result;
+  }
+
+  public async Task<IEnumerable<Hanzi>> GetAllInDb()
+  {
+    return await _hanziDbService.ReadAll();
+  }
+
+  public async Task<IEnumerable<Hanzi>> GetAllInDb(int skip, int take)
+  {
+    return await _hanziDbService.ReadRange(skip, take);
+  }
+
+  public async Task<bool> Delete(string id)
+  {
+    var deleted = await _unitOfWork.Hanzis.Delete(id);
+    await _unitOfWork.CompleteAsync();
+    return deleted;
+  }
+
+  #region Private Methods
+
+  private async Task<IEnumerable<string>> RemoveDuplicatedHanzis(IEnumerable<string> ids)
+  {
+    var whereIn = ids.ToArray();
+    // var existingHanzis = await _hanziDbService.ReadAll();
+    // var duplicatedOnes = existingHanzis
+    //     .Where(x => ids.Contains(x.Id)).ToList();
+    // foreach (var duplicatedOne in duplicatedOnes)
+    // {
+    //     await _unitOfWork.Hanzis.Delete(duplicatedOne.Id);
+    // }
+    var duplicatedOnes = await _unitOfWork.Hanzis.AllQuery()
+      .Where(x => whereIn.Contains(x.Id))
+      .ToListAsync();
+
+    if (duplicatedOnes.Any())
+    {
+      foreach (var duplicatedOne in duplicatedOnes)
+      {
+        await _unitOfWork.Hanzis.Delete(duplicatedOne.Id);
+      }
+    }
+
+    await _unitOfWork.CompleteAsync();
+    return ids;
+  }
+
+  #endregion
 }
