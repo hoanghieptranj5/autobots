38c997d93efc37c742816e0513d3818c87748179
Hiep Tran <hiep.tran@zoi.de>
2023-05-08 10:15:26 +0700

    removed Crawler project

--- Crawler/CollectorBase/ICollector.cs
+++
@@ -1,9 +0,0 @@
-using HtmlAgilityPack;
-
-namespace Crawler.CollectorBase;
-
-public interface ICollector<T> where T : class
-{
-  string Url { get; }
-  IEnumerable<T> DoCollectBusiness(HtmlDocument document);
-}
