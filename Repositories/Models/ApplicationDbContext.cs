de511894887c599c5ebe32bd13593adf580110b1
Hiep Tran <hiep.tran@zoi.de>
2023-09-02 08:54:41 +0700

    removed

--- Repositories/Models/ApplicationDbContext.cs
+++ Repositories/Models/ApplicationDbContext.cs
@@ -29,8 +29,7 @@ public partial class ApplicationDbContext : DbContext
 
     if (!optionsBuilder.IsConfigured)
     {
-      string connectionString = "Server=tcp:mysqlserverhip.database.windows.net,1433;Initial Catalog=autobot;Persist Security Info=False;User ID=azureuser;Password=Protoss5195;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-      optionsBuilder.UseSqlServer(connectionString);
+      
     }
   }
 }
