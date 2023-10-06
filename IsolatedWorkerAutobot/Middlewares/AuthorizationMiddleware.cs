6d30e412e12b55366f715f35cad694d0e3f75e6a
Hiep Tran <hiep.tran@zoi.de>
2023-10-06 16:02:03 +0700

    implement helper for writing response

--- IsolatedWorkerAutobot/Middlewares/AuthorizationMiddleware.cs
+++ IsolatedWorkerAutobot/Middlewares/AuthorizationMiddleware.cs
@@ -37,40 +37,32 @@ public class AuthorizationMiddleware : IFunctionsWorkerMiddleware
       var token = requestData.Query.Get(key);
       if (token == null)
       {
-        var req = await context.GetHttpRequestDataAsync();
-        var res = req!.CreateResponse();
-        res.StatusCode = HttpStatusCode.Unauthorized;
-        await res.WriteAsJsonAsync("Access Denied\n\nYou have attempted to access a protected resource that requires authentication using a JSON Web Token (JWT). To access this endpoint, you must include a valid JWT token in the request headers.\n\nPlease ensure that you have obtained an authentication token from our authentication service. You can do this by following our authentication process, which typically involves logging in and receiving a token in response.\n\nOnce you have obtained a valid JWT token, make sure to include it in your request headers as follows:\n\nAuthorization: Bearer {your_jwt_token_here}\n\nIf you believe you have received this message in error or need assistance with authentication, please contact our support team at support@example.com for further assistance.\n\nThank you for using our services.");
-        context.GetInvocationResult().Value = res;
+        const string message = "Access to this endpoint is unauthorized. Please provide a valid JSON Web Token (JWT) " +
+                               "in the 'Authorization header of your request.";
+        await MiddlewareResponseWriter.WriteAsJsonAsync(context, message, HttpStatusCode.Unauthorized);
         return;
       }
 
       var isValid = JwtHelper.ValidateToken(token);
       if (!isValid)
       {
-        var req = await context.GetHttpRequestDataAsync();
-        var res = req!.CreateResponse();
-        res.StatusCode = HttpStatusCode.Unauthorized;
-        await res.WriteAsJsonAsync("Invalid or Expired Token\n\nThe JSON Web Token (JWT) provided in the request is either invalid or has expired. To access this endpoint, you must provide a valid and unexpired JWT token.\n\nPossible reasons for this error include:\n- The token has expired: JWT tokens have a limited lifespan for security purposes. Please obtain a fresh token by re-authenticating.\n- The token signature is invalid: Ensure that the provided token is signed correctly and has not been tampered with.\n- The token format is incorrect: Verify that you have included the token in the request headers using the 'Authorization' header in the format 'Bearer {your_jwt_token_here}'.\n\nIf you believe you have received this message in error or need assistance with obtaining a valid token, please contact our support team at support@example.com for further assistance.\n\nThank you for using our services.");
-        context.GetInvocationResult().Value = res;
+        const string message = "Access to this endpoint is unauthorized due to an invalid or expired JSON Web Token (JWT).";
+        await MiddlewareResponseWriter.WriteAsJsonAsync(context, message, HttpStatusCode.Unauthorized);
         return;
       }
       
       var expiration = JwtHelper.GetClaim(token, ClaimTypes.Expiration);
       _logger.LogDebug(expiration);
-      DateTime.TryParse(expiration, out DateTime expirationDateTime);
+      DateTime.TryParse(expiration, out var expirationDateTime);
       _logger.LogDebug(expirationDateTime.ToShortDateString());
       if (expirationDateTime < DateTime.Now)
       {
-        var req = await context.GetHttpRequestDataAsync();
-        var res = req!.CreateResponse();
-        res.StatusCode = HttpStatusCode.Unauthorized;
-        await res.WriteAsJsonAsync("token expired.");
-        context.GetInvocationResult().Value = res;
-        return;
+        const string message = "Token has been expired. Please try to login again or use the refresh token instead.";
+        await MiddlewareResponseWriter.WriteAsJsonAsync(context, message, HttpStatusCode.Unauthorized);
       }
     }
-    else if (allowAllAttrs.Any()) _logger.LogDebug($"This method {targetMethod.Name} allow all inbounds.");
+    else if (allowAllAttrs.Any())
+      _logger.LogDebug($"This method {targetMethod.Name} allow all inbounds.");
 
     await next(context);
   }
---
+++ IsolatedWorkerAutobot/Middlewares/Helpers/MiddlewareResponseWriter.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace IsolatedWorkerAutobot.Middlewares.Helpers;
+
+public class MiddlewareResponseWriter
+{
+  public static async Task WriteAsJsonAsync<T>(FunctionContext context, T message, HttpStatusCode statusCode)
+  {
+    var req = await context.GetHttpRequestDataAsync();
+    var res = req!.CreateResponse();
+    res.StatusCode = statusCode;
+    
+    await res.WriteAsJsonAsync(message);
+    
+    context.GetInvocationResult().Value = res;
+  }
+}
