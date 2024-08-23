4f1111e9423507fcdcad7483c3559a7878c297e3
Hiep Tran <hiep.tran@zoi.de>
2024-08-23 20:06:24 +0700

    added getRandomList

--- IsolatedWorkerAutobot/Functions/HanziFunctions.cs
+++ IsolatedWorkerAutobot/Functions/HanziFunctions.cs
@@ -26,7 +26,33 @@ public class HanziFunctions
         string id,
         ILogger log)
     {
-        var result = await _hanziService.GetHanziInformationFromHvDic(id);
+        var result = await _hanziService.GetSingle(id);
         return new OkObjectResult(result);
+    }
+
+    [Authorize]
+    [OpenApiOperation("GetRandomList", "Hanzi", Summary = "GetRandomList",
+        Description = "This gets a random Hanzi list, length is passed by user.", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = AuthCode.Token, In = OpenApiSecurityLocationType.Header)]
+    [OpenApiParameter("length", Type = typeof(int))]
+    [OpenApiResponseWithBody(HttpStatusCode.OK, "text/plain", typeof(string),
+        Summary = "The response", Description = "This returns the response")]
+    [Function("GetRandomList")]
+    public async Task<IActionResult> GetRandomList(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hanzi/random/{length}")]
+        HttpRequestData req,
+        int length,
+        ILogger log)
+    {
+        try
+        {
+            var result = await _hanziService.GetRandomList(length);
+            return new OkObjectResult(result);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new StatusCodeResult((int)HttpStatusCode.BadRequest);
+        }
     }
 }
